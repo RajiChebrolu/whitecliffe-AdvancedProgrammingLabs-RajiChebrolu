80b7c4c5e060f7388023e80ce2c75c0866ce7006
rajichebrolu <raji.chebrolu@gmail.com>
2025-07-08 19:58:40 +1200

    Week6 Lab Unit Testing

---
+++ UnitTestProject/Test1.cs
@@ -0,0 +1,81 @@
+﻿namespace UnitTestProject
+{
+    [TestClass]
+    public class Test1
+    {
+        private const string Expected = "Hello World!";
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                UnitTestingLab.Program.Main();
+
+                var result = sw.ToString().Trim();
+                Assert.AreEqual(Expected, result);
+            }
+
+        }
+        [TestMethod]
+        public void AddTwoIntegers_ReturnSum()
+        {
+            var add = new UnitTestingLab.Program();
+            int expected = 8;
+            int result = add.Addition(5, 3);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void SubtractTwoIntegers_ReturnSub()
+        {
+            var sub = new UnitTestingLab.Program();
+            int expected = 3;
+            int result = sub.Subtraction(5, 2);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void DivideTwoIntegers_ReturnQuotient()
+        {
+            var div = new UnitTestingLab.Program();
+            double expected = 2.5;
+            double result = div.Division(5, 2);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void DivideByZero_ThrowException()
+        {
+            var d = new UnitTestingLab.Program();
+            d.Division(5, 0);
+        }
+
+        [TestMethod]
+        public void Factorial_Zero_ReturnsOne()
+        {
+            var fac = new UnitTestingLab.Program();
+            long result = fac.Factorial(0);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Factorial_PositiveNumber_ReturnsResult()
+        {
+            var fac = new UnitTestingLab.Program();
+            long expected = 120;
+            long result = fac.Factorial(5);
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        [ExpectedException (typeof(ArgumentException))]
+        public void Factorial_NegativeNUmber_ThrowsException()
+        {
+            var fac = new UnitTestingLab.Program();
+            fac.Factorial(-1);
+        }
+
+    }
+}
---
+++ UnitTestingLab/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace UnitTestingLab
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        virtual public int Addition(int a, int b)
+            { return a + b; }
+
+        virtual public int Subtraction(int a, int b) { return a - b; }
+
+        virtual public double Division(int a, int b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException("Cannot divide a number by zero.");
+            return (double)a / b;
+        }
+
+        public virtual long Factorial(int a)
+        {
+            if (a < 0)
+                throw new ArgumentException("Negative number inputs are not allowed.");
+            if (a == 0 || a == 1)
+                return 1;
+
+            long result = 1;
+            for (int i = 2; i <= a; i++)
+            {
+                result *=i;
+            }
+            return result;
+        }
+    }
+}
